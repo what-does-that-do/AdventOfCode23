6e1f0543371fb2adaf953d056cd05323d728c1b8
Oli <147245351+what-does-that-do@users.noreply.github.com>
2023-12-04 22:52:56 +0000

    Update part 2.py

--- Day 4/part 2.py
+++ Day 4/part 2.py
@@ -2,24 +2,32 @@ file = "Day 4/input.txt"
 f = open(file,'r')
 
 numbers = []
+lines = 0
 for line in f.readlines():
     text = line.split(':')[1].strip().replace('\n','')
     myNumbers = text.split('|')[1].split(' ')
     winningNumbers = text.split('|')[0].split(' ')
     numbers.append([winningNumbers,myNumbers])
+    lines += 1
 f.close()
 
-totalPoints = 0
+cardInfo = {}
 for set in numbers:
-    points = 0
+    wins = 0
     for win in set[0]:
         if win == '':
             continue
         if win in set[1]:
-            if points >= 1:
-                points *= 2
-            else:
-                points = 1
-    totalPoints += points
+            wins += 1
+    if set in cardInfo:
+        instances = cardInfo[set]
+    for win in range(wins):
+        if cardInfo[int(set) + win] in cardInfo:
+            cardInfo[int(set) + win] = cardInfo[int(set) + win] + 1
+        else:
+            cardInfo[int(set) + win] = 1
 
-print("Points =",totalPoints)
+totalCards = 0
+for c in cardInfo:
+    if c < lines:
+        totalCards += cardInfo[c]
