fc60e333547bcfea1e32592cad8e361c88955e06
Oli <147245351+what-does-that-do@users.noreply.github.com>
2023-12-05 10:15:07 +0000

    Finished part 2

--- Day 4/part 2.py
+++ Day 4/part 2.py
@@ -1,8 +1,10 @@
-file = "Day 4/test_case.txt"
+file = "Day 4/input.txt"
 f = open(file,'r')
 
 numbers = []
 lines = 0
+
+# Format lines
 for line in f.readlines():
     text = line.split(':')[1].strip().replace('\n','')
     myNumbers = text.split('|')[1].split(' ')
@@ -13,33 +15,32 @@ f.close()
 
 cardInfo = {}
 i = 0
+totalCards = 0
+
 for Set in numbers:
     if i in cardInfo:
         instances = cardInfo[i]
     else:
         instances = 1
-    print(instances,"of scratchcard",i)
-    for x in range(instances):
+    print("**",instances,"of scratchcard",i)
+    totalCards += instances
+    for x in range(instances): # repeat for each scratchcard
+        # Find number of wins
         wins = 0
         for win in Set[0]:
             if win == '':
                 continue
             if win in Set[1]:
                 wins += 1
-        print(wins,"wins")
+        #print(wins,"wins")
 
-
+        # Add number of instances of card to dict
         for win in range(wins):
             if int(i) + win + 1 in cardInfo:
-                cardInfo[int(i) + win+1] = cardInfo[int(i) + win + 1] + 1
+                cardInfo[int(i) + win+1] = cardInfo[int(i) + win+1] + 1
             else:
-                cardInfo[int(i) + win + 1] = 1
-            print("Added 1 to",i+win+1)
+                cardInfo[int(i) + win+1] = 2
+            #print("Added 1 to",int(i)+win+1)
     i += 1
-
-totalCards = 0
-for c in cardInfo:
-    if c < lines:
-        totalCards += cardInfo[c]
 
 print("Total cards =",totalCards)
